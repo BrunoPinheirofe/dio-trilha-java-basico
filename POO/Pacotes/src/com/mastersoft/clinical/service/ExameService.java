a3b00064985aa4fe07c5db858df2276625bf0dc2
BrunoPinheirofe <bruno2013pinheiro@gmail.com>
2023-09-19 09:58:47 -0300

    add Readme.md

---
+++ POO/Pacotes/src/com/mastersoft/clinical/service/ExameService.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mastersoft.clinical.service;
+
+import com.mastersoft.clinical.model.Exame;
+
+/**
+ *
+ * @author bruno
+ */
+public class ExameService {
+    
+    void salvarExame(Exame exame){
+    }
+    
+}
